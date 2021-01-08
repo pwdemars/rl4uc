a8649390dd2f10eac9eb317a40c8a3ab014b07a0
Patrick de Mars <pwdemars@gmail.com>
2021-01-08 14:49:07 +0000

    updated data retrieval and changed demand and wind scaling method

--- rl4uc/data_retrieval.py
+++ rl4uc/data_retrieval.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 
 import pandas as pd
+import numpy as np
 import os
 import requests
 import io
@@ -17,12 +18,18 @@ def get_wind_data(start_date, end_date, unit_id):
 	for date in date_range: 
 	    url = 'https://api.bmreports.com/BMRS/B1610/v2?APIKey={}&SettlementDate={}&Period=*&ServiceType=csv&NGCBMUnitID={}'.format(API_KEY, date, unit_id)
 	    response = requests.get(url, allow_redirects=True)
+
+	    # painful way of checking for an empty response...
+	    if 'Content-Disposition' not in response.headers:
+	    	print("Could not read {}".format(date))
+	    	continue 
+
 	    df = pd.read_csv(io.StringIO(response.content.decode('utf-8')), header=1).filter(COLUMNS)
 	    all_df = all_df.append(df)
 
 	# Sort and rename cols
 	all_df = all_df.sort_values(['Settlement Date', 'SP']).reset_index(drop=True)
-	all_df = all_df.rename(columns={'Settlement Date': 'date', 'SP': 'period', 'Quantity (MW)': 'wind_mw'})
+	all_df = all_df.rename(columns={'Settlement Date': 'date', 'SP': 'period', 'Quantity (MW)': 'wind'})
 	all_df.date = pd.to_datetime(all_df.date)
 
 	return all_df 
@@ -31,7 +38,7 @@ def get_demand_data(start_date, end_date):
 	date_range = pd.date_range(start_date, end_date).tolist()
 	date_range = [f.strftime('%Y-%m-%d') for f in date_range]
 
-	COLUMNS = ['date', 'period', 'demand_mw']
+	COLUMNS = ['date', 'period', 'demand']
 	all_df = pd.DataFrame(columns=COLUMNS)
 
 	for date in date_range:
@@ -40,7 +47,7 @@ def get_demand_data(start_date, end_date):
 		df = pd.read_csv(io.StringIO(response.content.decode('utf-8'))).reset_index()
 
 		# Rename columns
-		df = df.rename(columns={'level_0': 'type', 'level_1': 'date', 'HDR': 'period', 'SYSTEM DEMAND': 'demand_mw'})
+		df = df.rename(columns={'level_0': 'type', 'level_1': 'date', 'HDR': 'period', 'SYSTEM DEMAND': 'demand'})
 
 		# Get just ITSDO standing for Initial Transmission System Demand Outturn
 		df = df[df.type=='ITSDO']
@@ -66,45 +73,54 @@ if __name__=="__main__":
 	os.makedirs(SAVE_DIR, exist_ok=True)
 
 	WIND_UNIT_ID = 'WHILW-1' # specify the BM Unit ID to retrieve for wind data
-	WIND_PEN = 0.07 # desired wind penetration as a decimal
+	# WIND_PEN = 0.07 # desired wind penetration as a decimal. NOTE: it might be safer to scale for a max wind generation. 
 
-	# Dates for entire date set
-	start_date = '2015-01-01'
+	# Dates for entire data set
+	start_date = '2016-01-01'
 	end_date = '2019-12-31'
 
 	# Train/test split dates ([0] is beginning [1] is end)
-	train_dates = ('2015-01-01', '2018-12-31')
+	train_dates = ('2016-01-01', '2018-12-31')
 	test_dates = ('2019-01-01', '2019-12-31')
 
-	print("Getting demand data...")
-	demand_df = get_demand_data(start_date, end_date)
 	print("Getting wind data...")
 	wind_df = get_wind_data(start_date, end_date, WIND_UNIT_ID)
+	print("Getting demand data...")
+	demand_df = get_demand_data(start_date, end_date)
 
 	all_df = pd.merge(demand_df, wind_df, on=['date', 'period'])
+	all_df = all_df.dropna()
+
+	# Now we scale both demand and wind to fit the 10 generator problem. 
+	gen_info = pd.read_csv('data/kazarlis_units_10.csv')
+	max_demand = sum(gen_info.max_output) * 10/11 # allowing +10% headroom 
+	min_demand = sum(gen_info.max_output) * 0.3
+	all_df.demand = (all_df.demand - min(all_df.demand)) / (np.ptp(all_df.demand))
+	all_df.demand = all_df.demand * (max_demand - min_demand) + min_demand
+
+	# Scale wind 
+	max_wind = sum(gen_info.max_output) * 0.2
+	min_wind = 0
+	all_df.wind = (all_df.wind - min(all_df.wind)) / np.ptp(all_df.wind)
+	all_df.wind = all_df.wind * (max_wind - min_wind) + min_wind
+
+	# # Now we need to scale wind such that it meets the desired wind penetration throughout the data set 
+	# # Scale linearly by a factor of target total wind / current total wind 
+	# current_wind = sum(all_df.wind) 
+	# target_wind = sum(all_df.demand) * WIND_PEN
+	# all_df.wind = all_df.wind * target_wind / current_wind 
+
+	# Split train and test data
 	train_df = all_df[(all_df.date >= train_dates[0]) & (all_df.date <= train_dates[1])]
 	test_df = all_df[(all_df.date >= test_dates[0]) & (all_df.date <= test_dates[1])]
-
-	# Now we need to scale wind such that it meets the desired wind penetration throughout the data set 
-	# Scale linearly by a factor of target total wind / current total wind 
-	current_wind = sum(all_df.wind_mw) 
-	target_wind = sum(all_df.demand_mw) * WIND_PEN
-	all_df.wind_mw = all_df.wind_mw * target_wind / current_wind 
-
+	
 	# Check that wind never exceeds demand. 
-	assert(all(all_df.demand_mw > all_df.wind_mw)), "demand exceeds wind at some timesteps. turn down wind penetration"
+	assert(all(all_df.demand > all_df.wind)), "demand exceeds wind at some timesteps. turn down wind penetration"
+
+	# TODO: ensure that all days are complete and have exactly 48 periods 
+	# TODO: drop nans 
 
 	# Save to .csv
 	train_df.to_csv(os.path.join(SAVE_DIR, 'train_data.csv'), index=False)
 	test_df.to_csv(os.path.join(SAVE_DIR, 'test_data.csv'), index=False)
-
-
-
-
-
-
-
-
-
-
 
