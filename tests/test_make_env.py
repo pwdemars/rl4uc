3ab3834fff34ebe35e11396f012c375cba0cc10b
Patrick de Mars <pwdemars@gmail.com>
2021-03-29 16:15:55 +0100

    added test for making test env

--- tests/test_make_env.py
+++ tests/test_make_env.py
@@ -1,11 +1,19 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-import pytest
+import pandas as pd 
+import numpy as np
 
 from rl4uc.environment import make_env
 
-def test_make_env():
+def test_make_train_env():
 	for g in [5,10,20,30]:
-		make_env(num_gen=g)
-		
+		env = make_env(mode='train', num_gen=g)
+		obs = env.reset()
+
+def test_make_test_env():
+	df = pd.DataFrame({'demand': np.array([600., 550., 650.,]),
+				  'wind': np.array([10., 50., 70.])})
+	env = make_env(mode='test', profiles_df = df)
+	obs = env.reset()
+
