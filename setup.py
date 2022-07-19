c30c7d5a2fe699729e9fdb6b40a7fcbb19ced94a
Patrick de Mars <pwdemars@gmail.com>
2022-07-19 14:01:28 +0100

    Bump version

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ pkgs = {
 
 setuptools.setup(
     name="rl4uc", 
-    version="0.2.1",
+    version="0.2.2",
     author="Patrick de Mars",
     author_email="pwdemars@gmail.com",
     description="Reinforcement learning environment for the unit commitment problem",
